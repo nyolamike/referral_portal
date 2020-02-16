cc608fb663a37484c039cbe64eab7f7572cc4a97
nyola mike <iamnyola@gmail.com>
2020-02-16 06:35:11 -0800

    full3

--- application.js
+++ application.js
@@ -1,45 +1,48 @@
 var app = new Vue({
     el: '#application',
     data: {
-      message: 'Hello Vue!',
-      search_terms: "",
-      hospitals: [{
-          id:1,
-          name: "Mulago",
-          departments: [{
-              id: 1,
-              name: "Immunization Department",
-              description: "Immunization takes place from Monday to Friday and from 8am and usually ends by 2pm",
-              department_image: "---department_image---",
-              illustration_image: "---illustration_image---",
-              services:[{
-                  id: 1,
-                  name: "Poliomyelitis - OPV/IPV",
-                  description: "",
-                  service_image: "---service-image---",
-                  price: 0
-              },
-              {
+        message: 'Hello Vue!',
+        search_terms: "",
+        hospitals: [{
+            id: 1,
+            name: "Mulago",
+            departments: [{
                 id: 1,
-                name: "Poliomyelitis - OPV/IPV",
-                description: "",
-                service_image: "---service-image---",
-                price: 0
-            }
-            ]
-          }]
-      },{
-        id:2,
-        name: "Kirudu"
-    },{
-        id:3,
-        name: "Kawempe"
-    }]
+                is_opd: false,
+                name: "Immunization Department",
+                description: "Immunization takes place from Monday to Friday and from 8am and usually ends by 2pm",
+                department_image: "---department_image---",
+                illustration_image: "---illustration_image---",
+                services: [{
+                    id: 1,
+                    name: "Poliomyelitis - OPV/IPV",
+                    description: "",
+                    service_image: "---service-image---",
+                    price: 0
+                },
+                {
+                    id: 1,
+                    name: "Poliomyelitis - OPV/IPV",
+                    description: "",
+                    service_image: "---service-image---",
+                    price: 0
+                }
+                ]
+            },{
+                id:67
+            }]
+        }, {
+            id: 2,
+            name: "Kirudu"
+        }, {
+            id: 3,
+            name: "Kawempe"
+        }]
     },
-    methods:{
-        search(){
-            
+    methods: {
+        search() {
+
         }
     }
-  })
+})
   //0704195977
